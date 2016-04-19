df01b527f9b77a8950f528881aa45a166c8f9cc2
cjwit <chris.witulski@gmail.com>
2016-04-19 12:14:43 +0000

    initial setup for short-url

--- server.js
+++ server.js
@@ -1,27 +1,20 @@
 'use strict';
 
 var express = require('express');
-
 var app = express();
 
 app.get('/', function(req, res) {
 	res.sendFile(process.cwd() + '/public/index.html');
 });
 
-app.get('/:input', function(req, res) {
-    var input = req.params.input;
+app.get('/new/:url', function(req, res) {
+    var url = req.params.url;
+    res.send('create new entry for ', url);
+});
 
-    if (+input) {
-        input = +input
-    }
-    
-    var date = new Date( ( input === 'today' ? Date.now() : input ) )
-    var result = {
-        unix: date.getTime(),
-        natural: date.toDateString()
-    };
-
-    res.send(JSON.stringify(result));
+app.get('/:url', function(req, res) {
+    var url = req.params.url;
+    res.send('redirect to database entry for', url);
 });
 
 app.listen(process.env.PORT || 8080);
